254a700a15a2b544e026a292c38a9dafe4e23ef9
DELL <malecki.sebastian95@gmail.com>
2018-04-10 22:10:57 +0200

    Initial Commit

--- src/main/java/com/github/sebastianek12345/Aplication.java
+++ src/main/java/com/github/sebastianek12345/Aplication.java
@@ -1,14 +1,51 @@
 package com.github.sebastianek12345;
 
+import com.github.sebastianek12345.controller.PasswordSafeController;
+import com.github.sebastianek12345.model.PasswordEntry;
 import com.github.sebastianek12345.model.PasswordSafe;
+import com.google.gson.Gson;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Aplication {
 
     public static void main(String[] args){
 
         PasswordSafe passwordSafe = new PasswordSafe();
-        passwordSafe.addEntries("gmail","sebuś","sebastianek");
-        passwordSafe.show("gmail");
-        System.out.println(passwordSafe.show("gmail"));
+        PasswordEntry passwordEntry = new PasswordEntry(1,"sebo","super","daniel");
+        PasswordEntry passwordEntry1 = new PasswordEntry(2,"tomek","grejt","trep");
+        PasswordEntry passwordEntry2 = new PasswordEntry(3,"smarkacz","dupcia1234","mati");
+
+
+        PasswordSafeController passwordSafeController = new PasswordSafeController();
+        passwordSafeController.saveToFile(Arrays.asList(passwordEntry, passwordEntry1, passwordEntry2));
+
+        passwordSafeController.readFromFile("password-manager-file.pwm").forEach(System.out::println);
+
+
+
+
+
+
+//        PasswordEntry passwordEntry = new PasswordEntry(1,"w","twoja","ds");
+//        Gson gson = new Gson();
+//        String passwordEntryJson = gson.toJson(passwordEntry);
+//        System.out.println(passwordEntryJson);
+//
+//
+//        File file = new File("password-manager-file.pwm");
+//        try {
+//            FileUtils.writeStringToFile(file,passwordEntryJson,"UTF-8");
+//        }catch (IOException e){
+//            e.printStackTrace();
+//        }
+
+
     }
 }
